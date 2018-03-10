184b2b03e4005a6bb6fa57ac46db73886d02c8bb
Wout <wout.vanoevelen@student.kdg.be>
2018-03-10 13:53:29 +0100

    Auto stash before merge of "release" and "origin/release"

--- src/app/components/CRUD/Instrument/delete-instrument/delete-instrument.component.ts
+++ src/app/components/CRUD/Instrument/delete-instrument/delete-instrument.component.ts
@@ -1,6 +1,11 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+<<<<<<< Updated upstream:src/app/components/CRUD/Instrument/delete-instrument/delete-instrument.component.ts
 import {Instrument} from "../../../../model/instrument";
 import {InstrumentService} from "../../../../services/instrument/instrument.service";
+=======
+import {Instrument} from "../../../model/instrument";
+import {InstrumentService} from "../../../services/instrument.service/instrument.service";
+>>>>>>> Stashed changes:src/app/components/Instrument/delete-instrument/delete-instrument.component.ts
 
 @Component({
   selector: 'app-delete-instrument',
@@ -10,13 +15,16 @@ import {InstrumentService} from "../../../../services/instrument/instrument.serv
 export class DeleteInstrumentComponent implements OnInit {
   @Input() instrument: Instrument;
   @Output() deleted = new EventEmitter();
-  constructor(private instrumentService: InstrumentService) { }
+
+  constructor(private instrumentService: InstrumentService) {
+  }
 
   ngOnInit() {
   }
+
   public deleteInstrument(): void {
     this.instrumentService.deleteInstrument(this.instrument)
-      .subscribe();
-    this.deleted.emit();
+      .subscribe(next => console.log("next"), error => console.log("error"), () => this.deleted.emit())
+
   }
 }
