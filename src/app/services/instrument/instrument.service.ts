e6cd9f7be34f8f04892b2f60d52fac32e11efe55
Wout <wout.vanoevelen@student.kdg.be>
2018-03-22 14:19:18 +0100

    instrument calls updated

--- src/app/services/instrument/instrument.service.ts
+++ src/app/services/instrument/instrument.service.ts
@@ -5,29 +5,29 @@ import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class InstrumentService {
-  private path = 'http://127.0.0.1:8080';
+  private path = 'http://127.0.0.1:8080/instrument';
 
   constructor(private http: HttpClient) {
   }
 
   public createInstrument(newInstrument: Instrument): Observable<Instrument> {
-    return this.http.post<Instrument>(this.path + '/addinstrument', newInstrument);
+    return this.http.post<Instrument>(this.path + '/add', newInstrument);
   }
 
   public getInstrument(instrumentid: number): Observable<Instrument> {
-    return this.http.get<Instrument>(this.path + '/getinstrument/' + instrumentid);
+    return this.http.get<Instrument>(this.path + '/id/' + instrumentid);
   }
 
   public getInstruments(): Observable<Instrument[]> {
-    return this.http.get<Instrument[]>(this.path + '/getinstruments');
+    return this.http.get<Instrument[]>(this.path + '/get');
   }
 
   public editInstrument(instrument: Instrument) {
-    return this.http.put(this.path + '/editinstrument/' + instrument.id, instrument);
+    return this.http.put(this.path + '/edit/' + instrument.id, instrument);
   }
 
   public deleteInstrument(id: number) {
-    return this.http.delete(this.path + '/deleteinstrument/' + id);
+    return this.http.delete(this.path + '/delete/' + id);
 
   }
 }
