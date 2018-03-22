107fdd5770f229b5fb227b4398526e13792eaaa1
sam <samvdr@gmail.com>
2018-03-22 21:38:13 +0100

    details werkt ook (details,edit en new maar half)

--- src/app/components/_admin/lesson/details-lesson/details-lesson.component.ts
+++ src/app/components/_admin/lesson/details-lesson/details-lesson.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Lesson} from '../../../../model/lesson';
+import {LessonService} from '../../../../services/lesson/lesson.service';
 
 @Component({
   selector: 'app-details-lesson',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-lesson.component.css']
 })
 export class DetailsLessonComponent implements OnInit {
+  lesson = new Lesson();
 
-  constructor() { }
+  constructor(private lessonService: LessonService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.lessonService.getLesson(+params['id'])
+        .subscribe(lesson => this.lesson = lesson);
+    });
   }
-
 }
