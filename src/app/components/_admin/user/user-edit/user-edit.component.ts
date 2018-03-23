64e5179eecef64f82d5c42e0986a9aebc7ffa28f
Oli <olivier.b@telenet.be>
2018-03-23 04:31:32 +0100

    user edit

--- src/app/components/_admin/user/user-edit/user-edit.component.ts
+++ src/app/components/_admin/user/user-edit/user-edit.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {User} from "../../../../model/user";
 import {UserService} from "../../../../services/user/user.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {NgForm} from "@angular/forms";
 
 @Component({
@@ -11,21 +11,74 @@ import {NgForm} from "@angular/forms";
 })
 export class UserEditComponent implements OnInit {
   user: User;
+  options = [
+    {name: 'ROLE_LEERLING', checked:false},
+    {name: 'ROLE_LESGEVER', checked:false},
+    {name: 'ROLE_BEHEERDER', checked:false},
+  ];
 
   constructor(private userService: UserService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router,
+              private ref:ChangeDetectorRef) {
   }
 
   ngOnInit() {
-    // this.route.params.subscribe(params => {
-    //   this.userService.getUser(params['email'])
-    //     .subscribe(user => this.user = user);
-    // })
+    this.route.params.subscribe(params => {
+      this.userService.getUser(params['email'])
+        .subscribe(
+          resUser => {
+            this.user = resUser;
+
+            for (let role of this.user.roles) {
+              this.setChecked(role);
+            }
+
+            console.log(this.selectedRoles)
+            this.ref.detectChanges();
+          })
+    });
   }
 
-  // // region REST calls
-  // public editUser(userForm: NgForm): void {
-  //   this.userService.editUser(this.user)
-  //     .subscribe();
+  setChecked(role: string) {
+    this.options.map(obj => {
+      if (obj.name == role) {
+        console.log(obj.name + " checked")
+        obj.checked = true;
+      }
+    });
+  }
+
+  get selectedRoles() {
+    return this.options
+      .filter(opt => opt.checked)
+      .map(opt => opt.name)
+  }
+
+  public editUser(userForm: NgForm): void {
+    this.user.roles = this.selectedRoles
+    this.userService.editUser(this.user)
+      .subscribe(response => {
+               this.router.navigate(['/admin/users']);
+                window.alert(this.user.email + " is aangepast");
+              },
+              err => console.log(err.toString())
+            );
+  }
+
+  // public createUser(userForm: NgForm): void {
+  //   // for(let role of this.selectedRoles) {
+  //   //   console.log(role)
+  //   // }
+  //   this.newUser.roles = this.selectedRoles
+  //
+  //   this.userService.createUser(this.newUser)
+  //     .subscribe(response => {
+  //         this.router.navigate(['/admin/users']);
+  //         window.alert(this.newUser.email + " is aangemaakt");
+  //         userForm.reset();
+  //       },
+  //       err => console.log(err.toString())
+  //     );
   // }
 }
